1f63350605d0296bc1b85012c38402f4edbaa869
Jitesh4565 <73875204+Jitesh4565@users.noreply.github.com>
2023-11-17 13:17:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let userInput1=prompt("Enter Number");
+let userInput2=prompt("Enter Second Number");
+let num1=parseFloat(userInput1);
+let num2=parseFloat(userInput2);
+let result= 2*num1 + num2;
+ alert("The Final result is:"+ result);
+
+
