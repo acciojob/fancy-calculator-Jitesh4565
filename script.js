1664c56cc819cb8fd3f994936d2e67594eecf030
Jitesh4565 <73875204+Jitesh4565@users.noreply.github.com>
2023-11-17 13:24:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ let num1=parseFloat(userInput1);
 let num2=parseFloat(userInput2);
 let result= 2*num1 + num2;
  
-alert("The Final result is: "+ result);
+alert("The final result is: "+ result);
 
 
